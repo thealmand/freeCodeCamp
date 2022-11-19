26c1a9c6bfcd777ef8e8760335e493b55d87da18
thealmand <115939483+thealmand@users.noreply.github.com>
2022-11-19 16:55:13 -0600

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,73 +1,56 @@
 #! /bin/bash
 
-MAIN_MENU() {
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
-  echo -e "\n~~  Alex's Salon ~~\n"
-  echo -e "Let's get you scheduled girlfriend!"
-  echo -e "\nWhat service do you need?\n1) Slap n Tickle\n2) Mowhawk\n3) Fade"
-  read SERVICE_ID_SELECTED
+MAIN_PROMPT () {
 
-  case $SERVICE_ID_SELECTED in
-    1) SLAP ;;
-    2) MOWHAWK ;;
-    3) FADE ;;
-    *) MAIN_MENU "Please enter a valid option." ;;
-  esac
-
-
-}
-SLAP () {
-  SERVICE="Slap n Tickel"
-  GET_DEETS
+echo -e "\n How may I help you today?\n1) Cut\n2) Color\n3) Perm"
+GET_SERVICE
 }
 
-MOWHAWK () {
-  SERVICE="Mowhawk"
-  GET_DEETS
+
+GET_SERVICE () {
+read SERVICE_ID_SELECTED
+CHECK_SERVICE_ID=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+if [[ -z $CHECK_SERVICE_ID ]]
+then
+echo -e "\nPlease select a valid service."
+MAIN_PROMPT
+else
+  GET_NAME
+fi
 }
 
-FADE () {
-  SERVICE="Fade"
-  GET_DEETS
-}
+GET_NAME () {
 
-GET_DEETS () {
-  
-  echo -e "\nAwesome! What's your phone number"
-  read CUSTOMER_PHONE
-
-  # get customer id
-  CUSTOMER_ID=$(psql --username=freecodecamp --dbname=salon --tuples-only -c "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
-
-  # if no record of customer
-   if [[ -z $CUSTOMER_ID ]]
-   then
-  # get name
-  echo -e "\nSeems like we don't have record of you. Can I get your name please?"
-  read CUSTOMER_NAME
-  # insert into customer
-  INSERT_CUSTOMER_NAME=$(psql --username=freecodecamp --dbname=salon --tuples-only -c "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
-  #echo -e "\nNew customer added. Welcome $CUSTOMER_NAME $CUSTOMER_PHONE"
-  CUSTOMER_ID=$(psql --username=freecodecamp --dbname=salon --tuples-only -c "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
-  GET_TIME
-  else
-  
-   
-  CUSTOMER_NAME_UNFORMATED=$(psql --username=freecodecamp --dbname=salon --tuples-only -c "SELECT name FROM customers WHERE customer_id = $CUSTOMER_ID")
-  CUSTOMER_NAME=$(echo $CUSTOMER_NAME_UNFORMATED | sed 's/ |/"/' )
-  #echo "Customer found! $CUSTOMER_NAME"
-  GET_TIME
-  fi
+echo -e "\nMay I get your phone number please?"
+read CUSTOMER_PHONE
+CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+if [[ -z $CUSTOMER_NAME ]]
+then
+echo -e "\nMay I get your name please?"
+read CUSTOMER_NAME
+INSERT_NAME=$($PSQL "INSERT INTO customers(phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+GET_TIME
+else
+GET_TIME
+fi
 }
 
 GET_TIME () {
-  # get time
-  echo -e  "\nWhat time would you like?"
+
+CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+  echo -e "\nAnd what time would you like to schedule?"
   read SERVICE_TIME
-  # insert to appointments
-  INSERT_INTO_APPOINTMENTS=$(psql --username=freecodecamp --dbname=salon --tuples-only -c "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, '$SERVICE_ID_SELECTED', '$SERVICE_TIME')")
-  # output message
-  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
+  INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+  echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME." | sed 's/  / /' | sed 's/  / /'
+
 }
 
-MAIN_MENU
+
+
+echo -e "\n~~~~ Welcome to Alex's Salon ~~~~\n"
+MAIN_PROMPT
+
+
